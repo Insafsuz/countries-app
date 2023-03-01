d34011eedad8b7698edd017223b63e4d52a1770a
Insafsuz <in7afsuz@gmail.com>
2023-03-01 19:45:23 +0300

    theme switch

---
+++ src/components/Container.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  max-width: 1240px;
+  margin: 0px auto;
+  padding: 0 10px;
+`
---
+++ src/components/Header.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { Container } from './Container'
+import { FiMoon, FiSun } from 'react-icons/fi'
+
+const HeaderEl = styled.header`
+  padding: 28px 0;
+  background-color: var(--ui-color);
+  box-shadow: var(--shadow);
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Title = styled.h1`
+  font-weight: 600;
+`
+
+const ThemeIcon = styled.div`
+  display: flex;
+  align-items: center;
+  user-select: none;
+  cursor: pointer;
+  span {
+    margin-left: 10px;
+    text-transform: capitalize;
+  }
+`
+
+const Header: React.FC = () => {
+  const [theme, setTheme] = useState<string>('dark')
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme)
+  }, [theme])
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+
+  return (
+    <HeaderEl>
+      <Container>
+        <Wrapper>
+          <Title>Where is the world?</Title>
+          <ThemeIcon onClick={toggleTheme}>
+            {theme === 'dark' ? <FiMoon size={22} /> : <FiSun size={22} />}
+            <span>{theme} Theme</span>
+          </ThemeIcon>
+        </Wrapper>
+      </Container>
+    </HeaderEl>
+  )
+}
+
+export default Header
